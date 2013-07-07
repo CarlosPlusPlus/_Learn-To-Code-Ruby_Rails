e81b6137996c6edb719b57311c49dee61dae657c
cjlwired <carlos.jose.lazo@gmail.com>
2013-07-07 11:41:28 -0400

    Create student_site_rails app and create Student resource.

---
+++ flatironschool/student_site_rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudentSiteRails::Application.config.secret_token = '2d0e0e226f695b323f81e025978df940abe21d7f74ebf56b2d7f94b6e47c6e6c49f8d9af2a53a6a8cd10552ab84a77ab4acf610a1147971db255ca4c4260703f'
