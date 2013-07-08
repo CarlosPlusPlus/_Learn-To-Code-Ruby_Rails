43c4d3726a364f323a6e89fdcaabd8b2fe72f665
cjlwired <carlos.jose.lazo@gmail.com>
2013-07-08 16:32:35 -0400

    Add todolist rails app to repo.

---
+++ flatironschool/todolist_rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodolistRails::Application.config.secret_token = '54d15777cc4256fbf18d716cfd8840d9ced8893c509f483e6f6e94fdf7158013abfbaeecefee0344d8333933ee4ef138eea7f6944026ec6a8aec73a36de571c2'
