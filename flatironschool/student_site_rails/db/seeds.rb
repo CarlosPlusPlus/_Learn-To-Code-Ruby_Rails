a663822450a91ff70fa508563e54b7fb682d9212
cjlwired <carlos.jose.lazo@gmail.com>
2013-07-07 22:16:57 -0400

    Cleanup seeds.rb rake task.

--- flatironschool/student_site_rails/db/seeds.rb
+++ flatironschool/student_site_rails/db/seeds.rb
@@ -1,13 +1,5 @@
 require "#{Rails.root}/lib/student_scrape.rb"
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 scraped_students = StudentScrape.scrape_students.compact
 
   scraped_students.each do |s|
