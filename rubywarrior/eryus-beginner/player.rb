883cb8a3042b41932d0ef923d710b34c216f2c35
cjlwired <carlos.jose.lazo@gmail.com>
2013-07-14 13:15:42 -0400

    Make progress on Ruby Warrior, Level 04.

--- rubywarrior/eryus-beginner/player.rb
+++ rubywarrior/eryus-beginner/player.rb
@@ -4,35 +4,47 @@ class Player
 	# Player Initialization #
 	#########################
 
+	# Initialize instance variables.
 	def initialize
 		@action_taken 	= false		# Did I take my action?
 		@in_combat			= false		# Am I in combat?
-		@space_state		= false		# Is the space empty?
-		@warrior_health	= 20			# What is my health?
+		@space_empty		= false		# Is the space empty?
+		
+		@prv_health			= 20			# What was my previous health?
+		@cur_health			=	20			# What is my current health?
 	end
 
-
 	# List of possible warrior actions.
-	Actions = [:sense_environment,
-						 :check_health,
+	Actions = [:check_health,
 						 :attack_monster,
 						 :move_warrior]
 
-	##################
-	# Helper Methods #
-	##################
+	###########################
+	# Pre and Post Processing #
+	###########################
 
-	def sense_environment
-		@action_taken		= false		# Reset environment.
-		@space_state		= @warrior.feel.empty?
-		@warrior_health	= @warrior.health
+	def pre_sense
+  	# Reset warrior action from previous turn.
+		@action_taken		= false
 
-		# No longer in combat.
-		@in_combat = false if (@space_state == true)
+		# Determine health and surroundings.
+		@space_empty		= @warrior.feel.empty?
+		@cur_health			= @warrior.health
+
+		# Determine combat state.
+		@in_combat = in_combat?
 	end
 
+	def post_sense
+		@prv_health			= @warrior.health
+	end
+
+	##################
+	# Action Methods #
+	##################
+
 	def check_health
-		if (@warrior_health < 15) && (@in_combat == false)
+		if (@warrior_health < 15) && (@in_combat == :no)
 			@warrior.rest!
 			@action_taken = true
 		end
@@ -54,29 +66,48 @@ class Player
 		@action_taken = true
 	end
 
+	##################
+	# Helper Methods #
+	##################
+
+	# Did I lose health from last turn to this one?
+	def lost_health?
+		cur_health ~= prv_health ? true : false
+	end
+
+	# Am I in combat? If so, what "kind" of combat?
+	def in_combat?
+		combat_state = :no
+
+		:near if (@space_empty == false)
+		:far	if (@space_state == true) && (lost_health? == true)
+		
+		combat_state
+	end
+
+	#####################
+	# Logic for Actions #
+	#####################
+
+	def perform_action
+	end
+
 	####################
 	# Play Turn Method #
 	####################
 
-  def play_turn(warrior)
+	def take_turn
+		pre_sense				# Gather information on environment.
+		perform_action	# Perform action based on known items.
+		post_sense			# Store state of warrior for next turn.
+	end
 
+  def play_turn(warrior)
   	# Allow warrior to be accessed in all sections.
   	@warrior = warrior
 
-  	# Sense environment for clues.
-  	sense_environment
-
-  	# Check health of the warrior.
-  	check_health
-
-  	# Attack monster if present.
-  	attack_monster
-
-  	# Move warrior in a specific direction.
-  	move_warrior
-
-  	# Setup stage                                                    
-
+  	# Take turn for the warrior.
+  	take_turn
   end
 
 end
