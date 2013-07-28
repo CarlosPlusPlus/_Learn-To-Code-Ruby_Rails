338b6c4cea5fc953c56cdd1cde087dd2cd358deb
cjlwired <carlos.jose.lazo@gmail.com>
2013-07-27 22:55:14 -0400

    Add problem003 to project euler.

---
+++ project-euler/problem_003.rb
@@ -0,0 +1,25 @@
+# Problem 3
+# Largest prime factor
+
+@@target = 600851475143
+
+def is_prime(n)
+  2.upto(n - 1) do |i|
+    if n % i == 0
+        return false
+    end  
+  end  
+  return true  
+end
+
+def solve
+  (@@target**(1.0/2)).to_int.downto(2) do |i|
+    if (i % 2 != 0) && (@@target % i == 0) && is_prime(i)
+      return i
+    end  
+  end  
+end
+
+if __FILE__ == $0
+  puts solve
+end
