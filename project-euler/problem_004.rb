80cdc77a6e9eb8173c4554529e70e723fb2397a9
cjlwired <carlos.jose.lazo@gmail.com>
2013-08-19 14:52:58 -0400

    Solve problem 4 to Project Euler.

---
+++ project-euler/problem_004.rb
@@ -0,0 +1,32 @@
+# Problem 4
+# Largest palindrome product
+
+def palindrome?(num)
+  str = num.to_s
+
+  len = str.length
+  mid = (len / 2.0).ceil
+
+  for i in 0..mid
+    return false if str[i] != str[len-1-i] 
+  end
+
+  true
+end
+
+# Return maximum palindrome for 3 digit #s.
+def find_max_palindrome
+  max_palindrome = 0
+  
+  (100..999).each do |i|
+      (i..999).each do |j|
+      max_palindrome = i*j if (palindrome?(i*j) && (i*j > max_palindrome))
+    end
+  end
+
+  max_palindrome
+end
+
+# Solve problem.
+
+puts find_max_palindrome
