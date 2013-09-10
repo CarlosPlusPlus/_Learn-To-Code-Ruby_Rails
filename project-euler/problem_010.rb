3b810e1f866e1e314bc338bec1003e7fffca83d7
cjlwired <carlos.jose.lazo@gmail.com>
2013-09-09 21:33:48 -0400

    Solve Euler 010.

--- project-euler/problem_010.rb
+++ project-euler/problem_010.rb
@@ -3,4 +3,11 @@
 
 require 'prime'
 
-Float::INFINITY
+sum = 0
+
+Prime.each(Float::INFINITY) do |prime|
+	break if prime > 2E6;
+	sum += prime
+end
+
+puts "Sum of all primes under 2 million = #{sum}"
