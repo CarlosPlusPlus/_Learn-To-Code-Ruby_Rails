16360e6e8cb45ddad6365de96bb27cbe81ae9ffc
cjlwired <carlos.jose.lazo@gmail.com>
2013-09-19 21:38:33 -0400

    Update comments to Euler 017.

--- project-euler/problem_017.rb
+++ project-euler/problem_017.rb
@@ -3,10 +3,12 @@
 
 require 'numbers_and_words'
 
+# Use 'Numbers and Words' gem.
 def num_to_words(number)
 	word = I18n.with_locale(:en) { number.to_words }
 end
 
+# Sanitize string and take into account presence of 'and'.
 def get_length(word)
 	and_addition = word.include?(" hundred ") ? 3 : 0
 	clean_word 	 = word.gsub(/\s|-/,"")
@@ -14,7 +16,7 @@ def get_length(word)
 	clean_word.length + and_addition
 end
 
-
+# Collect letter count up to max_num.
 def count_letters(max_num)
 	(1..max_num).collect { |n| get_length(num_to_words(n)) }.inject(:+)
 end
