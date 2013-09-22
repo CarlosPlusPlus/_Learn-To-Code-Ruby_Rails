771f76d08fb01ebaa9c47267583351821d67f3ea
cjlwired <carlos.jose.lazo@gmail.com>
2013-09-22 10:16:29 -0400

    Plan out Euler 014.

--- project-euler/problem_014.rb
+++ project-euler/problem_014.rb
@@ -1,9 +1,12 @@
-# Problem 10
+# Problem 14
 # Longest Collatz Sequence
 
 class Collatz
 	CollatzSums = {}
 
-	def compute_sequence
+	def self.compute_sequence(num,count)
+		# If num exists in CollatzSum, return that key.
+		# If num == 1, return 1
+		# If num.even ? (1 + Collatz(n/2,n+1)) : (1 + Colatz(3n+1, n+1))
 	end
 end
