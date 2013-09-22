61d90146fe124ac76e834bf54ec50e13ec378f1b
cjlwired <carlos.jose.lazo@gmail.com>
2013-09-22 19:15:33 -0400

    Solve Euler 014 (pre-optimization).

--- project-euler/problem_014.rb
+++ project-euler/problem_014.rb
@@ -4,18 +4,31 @@
 class Collatz
 	Sums = {}
 
-	def self.compute_sequence(num,count)
-		
-		return (count + Sums[num]) if Sums[num]
-		
+	def self.compute_sequence(num,count=1)
 		if num == 1
 			Sums[num] = count
 			return count
 		end
 
-		num.even? ? compute_sequence(num/2,count+1) : compute_sequence(3*num+1,count+1)
+		return (count + Sums[num]) if Sums[num]
 
+		num.even? ? compute_sequence(num/2,count+1) : compute_sequence(3*num+1,count+1)
+	end
+
+	def self.sums
+		Sums
 	end
 end
 
-puts "Collatz value for 13 = #{Collatz.compute_sequence(13,1)}"
+#################
+# Solve Problem #
+#################
+
+max_val = [-1,-1]
+
+(1..1E6).each do |n|
+	count = Collatz.compute_sequence(n)
+	max_val = [n,count] if max_val[1] < count
+end
+
+puts "Maximum Collatz sequence: n = #{max_val[0]}, count = #{max_val[1]}"
