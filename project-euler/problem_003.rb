2a578cf076fd959bc1a59c6109fcd08d7c5089df
cjlwired <carlos.jose.lazo@gmail.com>
2013-10-03 08:09:28 -0400

    Refactor Euler 003 to not use class variables in global scope.

--- project-euler/problem_003.rb
+++ project-euler/problem_003.rb
@@ -1,8 +1,6 @@
 # Problem 3
 # Largest prime factor
 
-@@target = 600851475143
-
 def is_prime(n)
   2.upto(n - 1) do |i|
     if n % i == 0
@@ -12,14 +10,14 @@ def is_prime(n)
   return true  
 end
 
-def solve
-  (@@target**(1.0/2)).to_int.downto(2) do |i|
-    if (i % 2 != 0) && (@@target % i == 0) && is_prime(i)
+def solve(target)
+  (target**(1.0/2)).to_int.downto(2) do |i|
+    if (i % 2 != 0) && (target % i == 0) && is_prime(i)
       return i
     end  
   end  
 end
 
 if __FILE__ == $0
-  puts solve
+  puts solve(600851475143)
 end
