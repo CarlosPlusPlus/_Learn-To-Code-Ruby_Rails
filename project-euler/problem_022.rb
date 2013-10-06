aa4478ec773b693891ada93beb1e36215c2aa13c
cjlwired <carlos.jose.lazo@gmail.com>
2013-10-06 10:45:15 -0400

    Solve Euler 022.

--- project-euler/problem_022.rb
+++ project-euler/problem_022.rb
@@ -5,8 +5,18 @@ require 'awesome_print'
 
 def parse_names(path)
 	file = File.open(path).read
-	file.split(",").collect { |n| n.delete("\"") }
+	list = file.split(",").collect { |n| n.delete("\"") }
+	list.sort
+end
+
+def name_score(name,index)
+	name.bytes.inject(0) { |sum,b| sum + (b - 64) } * index
+end
+
+def list_score(names)
+	scores = names.each_with_index.collect { |n,i| name_score(n,i+1) }
+	scores.inject(:+)
 end
 
 names = parse_names('./_support/names.txt')
-ap names
+puts "Total List Score for names.txt = #{list_score(names)}"
