bf952bd7b38678033b37757f6e0fedd8dc384817
cjlwired <carlos.jose.lazo@gmail.com>
2013-10-06 10:22:40 -0400

    Euler 022 - get file parsing to work.

--- project-euler/problem_022.rb
+++ project-euler/problem_022.rb
@@ -1,3 +1,12 @@
 # Problem 22
 # Names Scores
 
+require 'awesome_print'
+
+def parse_names(path)
+	file = File.open(path).read
+	file.split(",").collect { |n| n.delete("\"") }
+end
+
+names = parse_names('./_support/names.txt')
+ap names
