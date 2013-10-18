57d15d50baaafb08c24db0cba308ee735ec1dbc4
cjlwired <carlos.jose.lazo@gmail.com>
2013-10-18 12:44:41 -0400

    Implement basic scrabble scoring.

--- gschool/scrabble-ruby/spec/scrabble_spec.rb
+++ gschool/scrabble-ruby/spec/scrabble_spec.rb
@@ -3,10 +3,26 @@ Bundler.require
 require './lib/scrabble'
 
 describe Scrabble do
-  describe ".score" do
+  describe "#score" do
+    
     it "scores a single letter" do
       expect( Scrabble.score("a") ).to eq 1
       expect( Scrabble.score("f") ).to eq 4
     end
+
+    it "scores a word" do
+    	expect( Scrabble.score("hello")  ).to eq 8
+    	expect( Scrabble.score("penguin")).to eq 10
+    end
+
+    it "handles empty or nil cases" do
+    	expect( Scrabble.score("") ).to eq 0
+    	expect( Scrabble.score(nil)).to eq 0
+    end
+
+    it "scores a word independent of case" do
+    	expect( Scrabble.score("wOrLD")).to eq 9
+    end
+
   end
 end
