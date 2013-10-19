c19c060d425910b16fe81d3e106b3f6491598eea
cjlwired <carlos.jose.lazo@gmail.com>
2013-10-19 19:50:07 -0400

    Update Scrabble code for small clerical changes.

--- gschool/scrabble-ruby/lib/scrabble.rb
+++ gschool/scrabble-ruby/lib/scrabble.rb
@@ -1,14 +1,13 @@
 class Scrabble
 	
 	MAX_LENGTH = 7
-
-	SCORES = {
-						"A"=>1, "B"=>3, "C"=>3, "D"=>2, "E"=>1, "F"=>4,
-						"G"=>2, "H"=>4, "I"=>1, "J"=>8, "K"=>5, "L"=>1,
-						"M"=>3, "N"=>1, "O"=>1, "P"=>3, "Q"=>10,"R"=>1,
-						"S"=>1, "T"=>1, "U"=>1, "V"=>4, "W"=>4, "X"=>8,
-						"Y"=>4, "Z"=>10
-					}
+	SCORES     = {
+						    "A"=>1, "B"=>3, "C"=>3, "D"=>2, "E"=>1, "F"=>4,
+						    "G"=>2, "H"=>4, "I"=>1, "J"=>8, "K"=>5, "L"=>1,
+						    "M"=>3, "N"=>1, "O"=>1, "P"=>3, "Q"=>10,"R"=>1,
+						    "S"=>1, "T"=>1, "U"=>1, "V"=>4, "W"=>4, "X"=>8,
+					      "Y"=>4, "Z"=>10
+					     }
 
   def self.score(word)
     return 0 if (word.nil? || word.empty?)
@@ -23,7 +22,6 @@ class Scrabble
   	win_words.size == 1 ? win_words.first : find_best_word(win_words)
   end
 
-  # Helper Methods
   private
 
   def self.find_best_word(words)
