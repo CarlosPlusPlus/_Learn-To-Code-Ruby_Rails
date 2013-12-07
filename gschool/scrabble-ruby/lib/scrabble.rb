31d8fc78925322498a50df0ebbf578ca6041cfab
cjlwired <carlos.jose.lazo@gmail.com>
2013-12-07 15:25:50 -0500

    Cleanup old code from scrabble model.

--- gschool/scrabble-ruby/lib/scrabble.rb
+++ gschool/scrabble-ruby/lib/scrabble.rb
@@ -46,6 +46,7 @@ end
 ########################################
 
 class Player
+
   attr_accessor :letters, :name, :score
 
   def initialize(name)
@@ -62,47 +63,5 @@ class Player
   def leading?(other_player)
     score > other_player.score
   end
-
-end
-
-<<-old
-
-class Scrabble
   
-  MAX_LENGTH = 7
-  SCORES     = {
-                "A"=>1, "B"=>3, "C"=>3, "D"=>2, "E"=>1, "F"=>4,
-                "G"=>2, "H"=>4, "I"=>1, "J"=>8, "K"=>5, "L"=>1,
-                "M"=>3, "N"=>1, "O"=>1, "P"=>3, "Q"=>10,"R"=>1,
-                "S"=>1, "T"=>1, "U"=>1, "V"=>4, "W"=>4, "X"=>8,
-                "Y"=>4, "Z"=>10
-               }
-
-  def self.score(word)
-    return 0 if (word.nil? || word.empty?)
-    word.upcase.chars.inject(0){|sum,c| sum + SCORES[c]}
-  end
-
-  def self.highest_score_from(words)
-    score_hash = Hash.new {|h,k| h[k] = [] }
-    words.each {|word| score_hash[score(word)] << word}
-
-    win_words = score_hash.max.flatten.drop(1) 
-    win_words.size == 1 ? win_words.first : find_best_word(win_words)
-  end
-
-  private
-
-  def self.find_best_word(words)
-    longest_length = words.max_by(&:length).length
-    
-    if longest_length == MAX_LENGTH
-      words.detect {|word| word.length == MAX_LENGTH}
-    else
-      words.sort_by(&:length).first
-    end
-  end
-
 end
-
-old
