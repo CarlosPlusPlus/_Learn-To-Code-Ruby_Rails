adc46931a8ead1a61dc8b2753a3ea3d0ab43d570
cjlwired <carlos.jose.lazo@gmail.com>
2013-12-16 20:05:21 -0500

    Add word_path problem to repo.

---
+++ code_problems/word_path.rb
@@ -0,0 +1,23 @@
+<<-INSTRUCTIONS
+
+Sneaking from Word to Word
+--------------------------
+
+Find a path from one word to another.
+
+A step is defined as:
+	- A change of a single letter in the word that produces another valid word.
+
+For bonus points, find the shortest path.
+
+**************************
+
+For example:
+	bat, man -> (bat, ban, man)
+
+Or a bit more fun:
+	grouper, twister -> (grouper, grouser, grosser, grasser, glasser, classer,
+										   clasher, flasher, flasker, flanker, franker, tranker,
+										   thanker, thinker, thinner, twinner, twinter, twister)
+
+INSTRUCTIONS
