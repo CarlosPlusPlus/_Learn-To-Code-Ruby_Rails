1dc506baa372f860e466672fa06ff6b3701bf437
Carlos Lazo <carlos.jose.lazo@gmail.com>
2015-04-28 09:07:13 -0400

    Fixes spacing with Project Euler 001.

--- project_euler/problem_001.rb
+++ project_euler/problem_001.rb
@@ -1,6 +1,6 @@
 # Problem 1
 # Multiples of 3 and 5
 
-sum = (1..999).select{|n| (n%3).zero? || (n%5).zero?}.inject(:+)
+sum = (1..999).select { |n| (n % 3).zero? || (n % 5).zero? }.inject(:+)
 
 puts "The sum of all the multiples of 3 or 5 below 1000 is #{sum}."
